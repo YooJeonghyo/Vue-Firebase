29569a4757f87366b8d4aec2c4f57490f0563a6f
YooJeonghyo <yjh5166@gmail.com>
2022-02-21 17:09:44 +0900

    navigation drawer 생성

--- vf_frontend/src/App.vue
+++ vf_frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon />
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-toolbar-title>Page title</v-toolbar-title>
       <v-spacer />
       <v-btn icon to="/about">
@@ -11,6 +11,16 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+    </v-navigation-drawer>
     <v-content>
       <router-view />
     </v-content>
@@ -25,8 +35,10 @@
 export default {
   name: 'App',
 
-  data: () => ({
-    //
-  })
+  data() {
+    return {
+      drawer: false
+    };
+  }
 };
 </script>
